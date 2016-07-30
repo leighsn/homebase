60e33280b9e54182e5ee6aa25193cad5942a5b75
Kellye Greene <kellye@gust.com>
2016-07-30 17:42:13 -0400

    some fixes to the code

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -4,16 +4,19 @@ $(function(){
     url: "http://opentable.herokuapp.com/api/restaurants?country=US",
 
   }).done(function(data){
-    data.restaurants.forEach(function(element){
+    if (data.restaurants.length > 0) {
+      data.restaurants.forEach(function(element){
 
-    var name = element.name,
+        var name = element.name,
         address = element.address,
         city = element.city,
         state = element.state,
         imageUrl = element.image_url
 
-      $('#restaurant_feed').append('<li><div class="thumb" style="background: url('+ element.image_url +') center center no-repeat; background-size: cover;"></div><h3>'+ element.name +'</h3><span>'+ element.addresss +'</span><span>'+ element.city +', '+ element.state +'</span></li>');
-    })
+        $('#restaurant_feed').append('<li><div class="thumb" style="background: url('+ element.image_url +') center center no-repeat; background-size: cover;"></div><h3>'+ element.name +'</h3><span>'+ element.address +'</span><br/><span>'+ element.city +', '+ element.state +'</span></li>');
+
+      })
+    }
   })
 })
 
