386b3895f0d72b28a9e4c3f27873607c7e4af48c
Kellye Greene <kellye@gust.com>
2016-08-01 00:24:10 -0400

    gets opentable results by zipcode

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -18,5 +18,23 @@ $(function(){
       })
     }
   })
-})
 
+  $("#user-form-submit").on("click", function(event){
+    var zipCode = $("#user-zipcode").val()
+    $("#restaurant_feed").empty();
+    $("#user-form input").empty();
+    event.preventDefault()
+  
+    $.ajax({
+      method: "GET",
+      url: `http://opentable.herokuapp.com/api/restaurants?zip=${zipCode}`,
+    }).done(function(data){
+        if (data.restaurants.length > 0) {
+          data.restaurants.forEach(function(element){
+              alert("ASfs")
+          $('#restaurant_feed').append('<li><div class="thumb" style="background: url('+ element.image_url +') center center no-repeat; background-size: cover;"></div><h3>'+ element.name +'</h3><span>'+ element.address +'</span><br/><span>'+ element.city +', '+ element.state +'</span></li>');
+        }) 
+      }
+    })
+  })
+})
