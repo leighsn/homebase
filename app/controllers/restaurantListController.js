421bd43ee862954a47139d75a02ee58cd8dfed5c
Kellye Greene <kellye@gust.com>
2016-08-02 11:40:39 -0400

    creates separate arrays for each users list of restaurants

--- app/controllers/restaurantListController.js
+++ app/controllers/restaurantListController.js
@@ -12,6 +12,7 @@ class RestaurantListController {
     }).done(function(data){
       $("#restaurant_feed").empty();
       $("#user-form input[type='text']").val("");
+      var restaurantArray = []
       if (data.restaurants.length > 0) {
         data.restaurants.forEach(function(element){
           var restaurantName = element.name,
@@ -23,7 +24,9 @@ class RestaurantListController {
             restaurantImage = element.image_url
           var restaurantList = new RestaurantList(restaurantName, restaurantAddress, restaurantCity, restaurantState, restaurantZip, restaurantLink, restaurantImage)
           $('#restaurant_feed').append('<li><div class="panel panel-default"><a href=' + restaurantLink + ' target="_blank"><div class="thumb" style="background: url('+ element.image_url +') center center no-repeat; background-size: cover;"></div><div class="panel-heading"><h3 class="panel-title">'+ element.name +'</h3></div></a><div class="panel-body"><span>'+ element.address +'</span><br/><span>'+ element.city +', '+ element.state +'</span></div></div></li>');
+          restaurantArray.push(restaurantList)
         })
+        Store.restaurantList.push(restaurantArray)
       }
     })
   }
